666f297d9db83f6af49718cce24582345221a6c2
alex950301 <102156589+alex950301@users.noreply.github.com>
2023-04-02 23:10:16 +0800

    Add files via upload

--- js/MainJs.js
+++ js/MainJs.js
@@ -48,19 +48,15 @@ button1.addEventListener("click", function() {
   content.innerHTML = `
     <div class="card">
       <div class="front">
-        <img src="picture/usb.jpg" width= 450px;/>
+        <img src="picture/poster.png" width= 450px;/>
       </div>
       <div class="back">
-        <p >
-          獎項 大獎_USB×1
-          </br/>
-          抽獎辦法 訂閱YouTube，以及在貼文下方留有@一位朋友的留言(是否要@越多人中獎機率越高?)
-          </br/>
-          時間 2/22公布(張貼海報+ig貼文)(時間配合連假)
-          </br/>
-          注意事項 再議
-          </br/>
-          聯繫方式 3/1(三) 公布得獎名單，請與三日之內用ig聯繫我們
+        <p style="color: white">
+        我們非常高興宣布，我們的獨家大獎是一個USB！只要在我們的YouTube頻道訂閱，並在貼文下方@一位朋友，
+        您就有機會贏得這個大獎！不管您@多少人，每位參加者只有一次機會贏得獎品，所以勝出的機率均等。
+        我們將在2/22公佈得獎名單，所以請留意我們的海報和ig貼文！
+        如果您是贏家，請在三日內與我們用ig聯繫，以便領取獎品。
+        再次感謝您參加我們的活動，祝您好運！
         </p>
       </div>
     </div>
@@ -149,8 +145,88 @@ button3.addEventListener("click", function() {
   `;
 });
 
-
 button4.addEventListener("click", function() {
+  content.innerHTML = `
+  <div>
+    <div>
+      <div class="method">
+        <div class="method-Main">
+          <div class="method-Main-icon">
+            <img class="Main" src="picture/photo-sticker.jpg" alt="css-icon" width="50px" height="50px" style="margin: 5px">
+            <p>新莊程研社-7th</p>
+          </div>
+          <div class="method-Main-info">
+
+            <div class="method-Main-info-left-div">
+              <div class="method-justify">
+
+                <div class="method-Main-info-div">
+                  <p class="method-Main-info-div2">Information</p>
+                </div>
+
+                <div class="method-Main-info-div">
+                  <div class="method-Main-info-div-text">
+                    <div class="method-Main-info-div-text-intro">
+                      <p class="method-Main-info-div-text-intro-style">Member</p>
+                    </div>
+                    <div style="display:grid;align-items: center;justify-content: center; letter-spacing: 3px;">
+                      <p class="method-Main-info-left1">社長 | 蕭建凱 |</p>
+                      <p class="method-Main-info-left1">副社 | 楊姿霖 |</p>
+                      <p class="method-Main-info-left1">教學 | 廖偉丞 |</p>
+                      <p class="method-Main-info-left1">總務 | 劉勁成 |</p>
+                      <p class="method-Main-info-left1">美宣 | 方子宸 |</p>
+                      <p class="method-Main-info-left1">文書 | 饒翊琦 |</p>
+                      <p class="method-Main-info-left1">公關 | 吳育全 |</p>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+
+            <div class="method-Main-info-right-div">
+              <div class="method-justify">
+
+
+                <div class="method-Main-info-div2">
+                  <p class="method-Main-info-right1">
+                  無論你是否熟悉程式語言，如果你想成為一位程式高手，那麼現在就加入程研社吧！
+                  這個社群不僅能幫助你了解程式碼的世界，還有完整且有趣的程式課程，你能夠累積實力，實現自己的夢想。
+                  此外，這裡還有各路好手，他們能和你一起切磋，讓你的程式技能得到更大的提升。
+                  歡迎加入程研社，和我們一起成為程式高手吧！
+                  </p>
+                </div>
+
+                <div class="method-Main-info-div2">
+                  <p class="method-Main-info-right1">
+                  每次社課都會錄影，供想學習的同學觀看。即便你不是社員，我們也歡迎你跟我們一起討論程式設計。
+                  此外，課程資源會同步在YouTube，記得要去追蹤訂閱喔。
+                  </p>
+                </div>
+
+                <div class="method-Main-info-div3">
+                  sdsd
+                </div>
+
+
+              </div>
+            </div>
+
+
+
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+});
+
+button5.addEventListener("click", function() {
   content.innerHTML = `
   <div>
     <div>
@@ -180,23 +256,7 @@ button4.addEventListener("click", function() {
   `;
 });
 
-button5.addEventListener("click", function() {
-  content.innerHTML = `
-    <div class="block3-right-box-monitor" style="background-color: yellow" >
-      <p>Button 1 was clicked</p>
-    </div>
-  `;
-});
-
 button6.addEventListener("click", function() {
-  content.innerHTML = `
-    <div class="block3-right-box-monitor" style="background-color: green" >
-      <p>Button 1 was clicked</p>
-    </div>
-  `;
-});
-
-button7.addEventListener("click", function() {
   content.innerHTML = `
     <div class="webinfo">
       <div class="webinfo-image">
@@ -210,6 +270,14 @@ button7.addEventListener("click", function() {
           Power by Github
         </p>
       </div>
+    </div>
+  `;
+});
+
+button7.addEventListener("click", function() {
+  content.innerHTML = `
+    <div class="block3-right-box-monitor" style="background-color: green" >
+      <p>Button 1 was clicked</p>
     </div>
   `;
 });
---
+++ js/popcat.js
@@ -0,0 +1,39 @@
+const popcat = document.querySelector("#popcat");
+const btn = document.querySelector("#button7");
+
+const openMouthImg = "picture/open.png";
+const closeMouthImg = "picture/close.png";
+
+const openMouthSound = new Audio("sound/open.mp3");
+const closeMouthSound = new Audio("sound/close.mp3");
+
+btn.addEventListener("mousedown", openMouth);
+btn.addEventListener("mouseup", closeMouth);
+
+btn.addEventListener("touchstart", function(event) {
+    event.preventDefault();
+    openMouth();
+})
+
+btn.addEventListener("touchend", function(event) {
+    event.preventDefault();
+    closeMouth();
+})
+
+function openMouth() {
+    popcat.src = openMouthImg;
+    openMouthSound.play();
+}
+
+function closeMouth() {
+    popcat.src = closeMouthImg;
+    closeMouthSound.play();
+}
+
+/*
+因為當用戶觸摸按鈕時，touchstart事件處理程序會立即切換彈出貓的圖像和播放聲音，
+而當用戶停止觸摸按鈕時，touchend事件處理程序也會立即切換彈出貓的圖像和播放聲音。
+但是，因為touchstart和touchend事件可能被延遲或丟失，所以最好使用與桌面上的滑鼠事件相同的事件處理程序mousedown和mouseup
+，這樣可以確保在所有情況下都能正確地處理用戶的輸入。
+同時，透過preventDefault()方法來禁止觸摸屏上的滑動行為，避免因手指滑動而導致不必要的觸發。
+*/
